9bfd7266e230f209af521ed95be76af2ee43c831
VladyslavYaushev <i.vlad.yaushev@gmail.com>
2025-09-18 13:17:02 +0300

    fix logs for loops

--- conditionsAndLoops/task3.js
+++ conditionsAndLoops/task3.js
@@ -2,15 +2,15 @@
 const number = 2
 
 for (let i = 1; i <=10; i++) {
-    console.log(number * i)
+    console.log( `${number} x ${i} = ` + number * i)
 }
 
-//loop while
+// loop while
 const secondNumber = 3
 let counter = 1
 
 while(counter <= 10){
-    console.log(secondNumber * counter)
+    console.log(`${secondNumber} x ${counter} = ` + secondNumber * counter)
     counter++
     if(counter > 10){
         counter = 0
