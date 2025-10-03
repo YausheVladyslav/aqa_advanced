994c0afb9ee205529d6d1a7bf75dd07ced1a0704
VladyslavYaushev <i.vlad.yaushev@gmail.com>
2025-10-03 16:52:00 +0300

    complete arrays tasks

---
+++ arrays/task3.js
@@ -0,0 +1,4 @@
+const numbers = [10, 20, 30, 40, 50, 60]
+const numbersSum = numbers.reduce((previousValue, currentValue) => previousValue + currentValue)
+
+console.log(numbersSum)
---
+++ arrays/task5.js
@@ -0,0 +1,4 @@
+const firstArray = [1,3,5]
+const secondArray = [10,15,20]
+
+console.log(firstArray.concat(secondArray))
